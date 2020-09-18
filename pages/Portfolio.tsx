ca1b7903b96097b6a4984c66abdad0ab9c9f3fbb
pooria <pooriavakili09@gmail.com>
2020-09-18 23:26:34 +0430

    firstcommit

--- pages/Portfolio.tsx
+++ pages/Portfolio.tsx
@@ -1,4 +1,5 @@
 // import { GetStaticProps } from "next";
+import WorkExperience from "./WorkExperience"
 interface AppProps {}
 const Portfolio: React.FunctionComponent<AppProps> = ({}) => {
   return (
@@ -105,54 +106,7 @@ const Portfolio: React.FunctionComponent<AppProps> = ({}) => {
                 </div>
               </div>
             </div>
-            <div className="tab-pane" id="graphic-design" role="tabpanel">
-              <div className="ml-auto mr-auto">
-                <div className="row">
-                  <div className="col-md-6">
-                    <div
-                      className="cc-porfolio-image img-raised"
-                      data-aos="fade-up"
-                      data-aos-anchor-placement="top-bottom"
-                    >
-                      <a href="https://zhoon.ir/">
-                        <figure className="cc-effect">
-                          <img src="images/zhoon.png" alt="Image" />
-                          <figcaption>
-                            <div className="h4">tree month work experience</div>
-                            <p>zhoon</p>
-                          </figcaption>
-                        </figure>
-                      </a>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div
-                      className="cc-porfolio-image img-raised"
-                      data-aos="fade-up"
-                      data-aos-anchor-placement="top-bottom"
-                    ></div>
-                    <div
-                      className="cc-porfolio-image img-raised"
-                      data-aos="fade-up"
-                      data-aos-anchor-placement="top-bottom"
-                    >
-                      <a href="https://drkarafarini.com/">
-                        <figure className="cc-effect">
-                          <img src="images/drkarafariny.png" alt="Image" />
-                          <figcaption>
-                            <div className="h4">
-                              {" "}
-                              seven month work experience
-                            </div>
-                            <p>drkarafarini</p>
-                          </figcaption>
-                        </figure>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+        <WorkExperience/>
           </div>
         </div>
       </div>
---
+++ pages/WorkExperience.tsx
@@ -0,0 +1,63 @@
+
+interface AppProps {
+
+}
+const WorkExperience: React.FunctionComponent<AppProps> = ({
+
+}) => {
+  return (
+    
+    <>
+      <div className="tab-pane" id="graphic-design" role="tabpanel">
+<div className="ml-auto mr-auto">
+  <div className="row">
+    <div className="col-md-6">
+      <div
+        className="cc-porfolio-image img-raised"
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom"
+      >
+        <a href="https://zhoon.ir/">
+          <figure className="cc-effect">
+            <img src="images/zhoon.png" alt="Image" />
+            <figcaption>
+              <div className="h4">tree month work experience</div>
+              <p>zhoon</p>
+            </figcaption>
+          </figure>
+        </a>
+      </div>
+    </div>
+    <div className="col-md-6">
+      <div
+        className="cc-porfolio-image img-raised"
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom"
+      ></div>
+      <div
+        className="cc-porfolio-image img-raised"
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom"
+      >
+        <a href="https://drkarafarini.com/">
+          <figure className="cc-effect">
+            <img src="images/drkarafariny.png" alt="Image" />
+            <figcaption>
+              <div className="h4">
+                {" "}
+                seven month work experience
+              </div>
+              <p>drkarafarini</p>
+            </figcaption>
+          </figure>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+    </>
+  )
+
+}
+export default WorkExperience;
